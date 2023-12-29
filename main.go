3340afde6329ce214d8ef97dc2f1f04cc10f4248
ito0804takuya <ito0804takuya@gmail.com>
2023-12-29 23:52:16 +0900

    テストしやすいようrun関数を切り出し

--- main.go
+++ main.go
@@ -1,20 +1,47 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
 	"net/http"
-	"os"
+
+	"golang.org/x/sync/errgroup"
 )
 
 func main() {
-	err := http.ListenAndServe(
-		":18080",
-		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	// テストしやすいようにrun関数に切り出した
+	if err := run(context.Background()); err != nil {
+		log.Print("failed to terminate server: %v", err)
+	}
+}
+
+func run(ctx context.Context) error {
+	s := &http.Server{
+		Addr: ":18080",
+		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
 		}),
-	)
-	if err != nil {
-		fmt.Printf("failed to terminate server: %v", err)
-		os.Exit(1)
 	}
+
+	eg, ctx := errgroup.WithContext(ctx)
+
+	// 別ゴルーチンでHTTPサーバーを起動
+	eg.Go(func() error {
+		// ErrServerClosedで意図的に終了した場合を除く
+		if err := s.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Printf("failed to close: %+v", err)
+			return err
+		}
+		return nil
+	})
+
+	// キャンセル通知を待機
+	<-ctx.Done()
+	// キャンセル通知を受け取ったら終了
+	if err := s.Shutdown(context.Background()); err != nil {
+		log.Printf("failed to shutdown: %+v", err)
+	}
+
+	return eg.Wait()
 }
