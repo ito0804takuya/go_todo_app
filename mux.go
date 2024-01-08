9f7ae1cb244f6836a7ef27d548d53ddc7809605c
ito0804takuya <ito0804takuya@gmail.com>
2024-01-08 17:24:25 +0900

    タスク一覧、登録ハンドラーをルーティングに登録

--- mux.go
+++ mux.go
@@ -1,17 +1,33 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-playground/validator"
+	"github.com/ito0804takuya/go_todo_app/handler"
+	"github.com/ito0804takuya/go_todo_app/store"
+)
 
 // muxはマルチプレクサのことで、複数の入力を単一の出力に結合するための装置のこと。
 
 func NewMux() http.Handler {
-	mux := http.NewServeMux()
+	mux := chi.NewRouter()
 
 	// ヘルスチェック用
 	mux.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json: charset=utf-8")
 		_, _ = w.Write([]byte(`{"status": "ok"}`))
 	})
+
+	// AddTask
+	v := validator.New()
+	at := &handler.AddTask{Store: store.Tasks, Validator: v}
+	mux.Post("/tasks", at.ServerHTTP)
+
+	// ListTask
+	lt := &handler.ListTask{Store: store.Tasks}
+	mux.Get("/tasks", lt.ServeHTTP)
 
 	return mux
 }
