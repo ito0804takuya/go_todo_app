5e719753ca72d0ecb3c53aa88815903fbceb3292
ito0804takuya <ito0804takuya@gmail.com>
2024-01-08 21:24:53 +0900

    DBのマイグレーション作成

---
+++ _tools/mysql/schema.sql
@@ -0,0 +1,19 @@
+CREATE TABLE `user` (
+  `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'ユーザーID',
+  `name` VARCHAR(20) NOT NULL COMMENT 'ユーザー名',
+  `password` VARCHAR(80) NOT NULL COMMENT 'パスワードハッシュ',
+  `role` VARCHAR(80) NOT NULL COMMENT 'ロール',
+  `created` DATETIME(6) NOT NULL COMMENT '作成日時',
+  `modified` DATETIME(6) NOT NULL COMMENT '更新日時',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `uix_name` (`name`) USING BTREE
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COMMENT = 'ユーザー';
+
+CREATE TABLE `task` (
+  `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'タスクID',
+  `title` VARCHAR(128) NOT NULL COMMENT 'タイトル',
+  `status` VARCHAR(20) NOT NULL COMMENT 'タスクの状態',
+  `created` DATETIME(6) NOT NULL COMMENT '作成日時',
+  `modified` DATETIME(6) NOT NULL COMMENT '更新日時',
+  PRIMARY KEY (`id`)
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COMMENT = 'タスク';
