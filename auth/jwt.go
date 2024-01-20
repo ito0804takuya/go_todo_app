194570ac7b97c7b9285fda3d1f123c366b98c533
ito0804takuya <ito0804takuya@gmail.com>
2024-01-20 18:01:04 +0900

    JWTer実装]

--- auth/jwt.go
+++ auth/jwt.go
@@ -1,7 +1,13 @@
 package auth
 
 import (
+	"context"
 	_ "embed"
+	"fmt"
+
+	"github.com/ito0804takuya/go_todo_app/clock"
+	"github.com/ito0804takuya/go_todo_app/entity"
+	"github.com/lestrrat-go/jwx/jwk"
 )
 
 // NOTE: go:embedによって実行バイナリにpemファイルを埋め込むことができる。そうすることでシングルバイナリで実行可能になる
@@ -11,3 +17,40 @@ var rawPrivKey []byte
 
 //go:embed cert/public.pem
 var rawPubKey []byte
+
+type JWTer struct {
+	PrivateKey, PublicKey jwk.Key
+	Store Store
+	Clocker clock.Clocker
+}
+
+//go:generate go run github.com/matryer/moq -out moq_test.go . Store
+type Store interface {
+	Save(ctx context.Context, key string,  userID entity.UserID) error
+	Load(ctx context.Context, key string) (entity.UserID, error)
+}
+
+func NewJWTer(s Store) (*JWTer, error) {
+	j := &JWTer{Store: s}
+	privkey, err := parse(rawPrivKey)
+	if err != nil {
+		return nil, fmt.Errorf("failed in NewJWTer: private key: %w", err)
+	}
+	pubkey, err := parse(rawPubKey)
+	if err != nil {
+		return nil, fmt.Errorf("failed in NewJWTer: public key: %w", err)
+	}
+	j.PrivateKey = privkey
+	j.PublicKey = pubkey
+	j.Clocker = clock.RealClocker{}
+	return j, nil
+}
+
+// pemファイルのbyte列 → jwt.Keyに変換
+func parse(rawKey []byte) (jwk.Key, error) {
+	key, err := jwk.ParseKey(rawKey, jwk.WithPEM(true))
+	if err != nil {
+		return nil, err
+	}
+	return key, nil
+}
--- go.mod
+++ go.mod
@@ -5,16 +5,25 @@ go 1.20
 require (
 	github.com/DATA-DOG/go-sqlmock v1.5.2
 	github.com/caarlos0/env/v6 v6.10.1
+	github.com/go-redis/redis/v8 v8.11.5
 	github.com/google/go-cmp v0.6.0
+	github.com/lestrrat-go/jwx v1.2.28
 	github.com/matryer/moq v0.3.3
+	golang.org/x/crypto v0.18.0
 	golang.org/x/sync v0.6.0
 )
 
 require (
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
+	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.2.0 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
-	github.com/go-redis/redis/v8 v8.11.5 // indirect
-	golang.org/x/crypto v0.18.0 // indirect
+	github.com/goccy/go-json v0.10.2 // indirect
+	github.com/lestrrat-go/backoff/v2 v2.0.8 // indirect
+	github.com/lestrrat-go/blackmagic v1.0.2 // indirect
+	github.com/lestrrat-go/httpcc v1.0.1 // indirect
+	github.com/lestrrat-go/iter v1.0.2 // indirect
+	github.com/lestrrat-go/option v1.0.1 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
 	golang.org/x/mod v0.14.0 // indirect
 	golang.org/x/tools v0.17.0 // indirect
 	gopkg.in/go-playground/assert.v1 v1.2.1 // indirect
